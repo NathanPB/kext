cfafa2ac5b9c989a074936b5171aed90ccdc9c3b
Nathan <nathan.bombana@ewally.com.br>
2022-02-25 11:03:18 -0300

    Moving chainer documentation around

--- src/chainer.ts
+++ src/chainer.ts
@@ -8,24 +8,25 @@
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-/**
- * Takes `value` and apply it to `next`. Use it's result to create a new chainer.
- * @param next
- * @see chainer
- */
 type NextChainer<I> = <O> (next: (input: I)=>O) => Chainer<O>
 
-/**
- * Takes `value` and apply it to `next`. Use it's result to create a promise with.
- * @param next
- */
 type NextPromise<I> = <O> (next: (input: I)=>Promise<O> | O) => Promise<O>
 
 /**
  * Used to chain multiple function calls in a curry-like style.
  */
 export type Chainer<I> = {
+  /**
+   * Takes `value` and apply it to `next`. Use it's result to create a new chainer.
+   * @param next
+   * @see chainer
+   */
   next: NextChainer<I>
+
+  /**
+   * Takes `value` and apply it to `next`. Use it's result to create a promise with.
+   * @param next
+   */
   then: NextPromise<I>
 
   /**
